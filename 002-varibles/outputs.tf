6bcd9d94bca92ce83b6690677316979663b73427
Mukta06 <cbmukta06@gmail.com>
2024-07-19 00:41:46 +0530

    Terraform  Auto Push

--- 002-varibles/outputs.tf
+++ 002-varibles/outputs.tf
@@ -1,4 +1,11 @@
 # Printing variable values
+# Declare all the veriables in vars.tf file then terraform will recognize the file no need to explicitly mention
+# If we have any other name of varibale file then we need to create empty varibale for that before printing --> file name should be terraform.tfvars
+# If the veriable file name is different like dev.tfvars or prod.tfvars etc then the CLI is :
+# terraform plan --var-file=dev.tfvars; terraform apply --var-file=dev.tfvars -auto-approve
+
+
+
 
 variable "city" {}
 output "Fav_City_Name"{
