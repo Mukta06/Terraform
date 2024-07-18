ddceb357e8e6c02e62c2ad40ebad79e47e0f7252
Mukta06 <cbmukta06@gmail.com>
2024-07-19 00:53:57 +0530

    Terraform  Auto Push

--- 003-ec2/ec2.tf
+++ 003-ec2/ec2.tf
@@ -1,2 +1,16 @@
 # Provider --> 
 provider "aws"{}
+
+resource "aws_instance" "Application" {
+    ami= "ami-072983368f2a6eab5"
+    instance_type = "t2.micro"
+    tags = {
+      Name="tf-instance-dev"
+    }
+  
+}
+
+output "Private_IP" {
+    value="Private-IP Address of the created instance is : ${aws_instance.Application.private_ip}"
+  
+}
--- 003-ec2/provider.tf
+++ 003-ec2/provider.tf
@@ -1,17 +1,4 @@
-provider "aws" {
+#provider "aws" {
   # Configuration options
-}
+#}
 
-resource "aws_instance" "Application" {
-    ami= "ami-072983368f2a6eab5"
-    instance_type = "t2.micro"
-    tags = {
-      Name="tf-instance-dev"
-    }
-  
-}
-
-output "Private_IP" {
-    value="Private-IP Address of the created instance is : ${aws_instance.Application.private_ip}"
-  
-}
