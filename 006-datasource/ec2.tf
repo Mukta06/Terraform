73d3f21bc49faf303545ab130fe460888615f5f7
Mukta06 <cbmukta06@gmail.com>
2024-07-20 22:54:21 +0530

    Terraform  Auto Push

--- 006-datasource/ec2.tf
+++ 006-datasource/ec2.tf
@@ -1,3 +1,8 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-Centos-8"
+  owners           = ["355449129696"]
+}
 resource "aws_instance" "Web"{
     ami="data.aws_ami.ami.id"
     instance_type="t2.micro"
@@ -6,11 +11,7 @@ resource "aws_instance" "Web"{
     }
 }
 
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "DevOps-LabImage-Centos-8"
-  owners           = ["355449129696"]
-}
+
 
 output "private_ip_address" {
     value="IP Address of the created instance is : ${aws_instance.Web.private_ip}"
