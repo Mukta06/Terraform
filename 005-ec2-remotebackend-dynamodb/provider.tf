a6d813dcf92e7987cd3f0b738a696ddb217d75b8
Mukta06 <cbmukta06@gmail.com>
2024-07-20 21:45:06 +0530

    Terraform  Auto Push

--- 005-ec2-remotebackend-dynamodb/provider.tf
+++ 005-ec2-remotebackend-dynamodb/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {}
 terraform {
   backend "s3" {
     bucket = "b57-mukta-tf-state-bucket"
-    key = "/dev/dynamodb/terraform.tfstate"
+    key = "dev/dynamodb/terraform.tfstate"
     region="us-east-1"
     dynamodb_table = "terraform-locking"
     
