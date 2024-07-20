856e588406c52a220e3fe68fbab52bd78a4b58ca
Mukta06 <cbmukta06@gmail.com>
2024-07-20 23:07:30 +0530

    Terraform  Auto Push

--- 006-datasource/ec2.tf
+++ 006-datasource/ec2.tf
@@ -1,15 +1,43 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "DevOps-LabImage-Centos-8"
-  owners           = ["355449129696"]
-}
 resource "aws_instance" "Web"{
     ami=data.aws_ami.ami.id
     instance_type="t2.micro"
     tags={
         Name="tf-datasource-instance"
     }
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-Centos-8"
+  owners           = ["355449129696"]
+}
+
+resource "aws_security_group" "allow_tls" {
+  name   = "b57-allow_ssh"
+  description = "Allow SSH inbound and outbound traffic"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags={
+    Name="allow_ssh_sg"
+  }
+}
+
+
 
 
 
